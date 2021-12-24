df2d0d0b643578a039c0d5107cc734eaad462799
forJihye <qkrwlgp76@gmail.com>
2021-12-24 13:56:32 +0900

    (selector.ts) DOM 셀렉터 상수

---
+++ src/selector.ts
@@ -0,0 +1,15 @@
+const S = Object.freeze({
+  ROUTE_INTRO: 'route-intro',
+  ROUTE_EDIT: 'route-edit',
+  ROUTE_ENDING: 'route-ending',
+
+  POPUP_UPLOAD_ERROR: 'popup-upload-error',
+  POPUP_IMAGE_WRAN: 'popup-image-warn',
+  POPUP_IMAGE_UPLOAD: 'popup-image-upload',
+
+  IMAGE_CANVAS: 'image-canvas',
+  IMAGE_UPLOAD: 'image-upload',
+  IMAGE_ROTATE: 'image-rotate'
+})
+
+export default S;
